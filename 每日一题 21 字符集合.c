a345c4ad68833b1e6074aaf8d30a6b6425d20598
IWANTBAT <18729932977@163.com>
2020-07-17 15:49:29 +0800

    标记数组

---
+++ 每日一题 21 字符集合.c
@@ -0,0 +1,26 @@
+﻿//字符集合
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	while (cin >> str){
+		int l = str.length();
+		int a[123] = { 0 };
+		for (int i = 0; i < l; i++){
+			a[str[i]] = 1;
+		}
+		for (int i = 0; i < l; i++){
+			if (a[str[i]] == 1){
+				cout << str[i];
+				a[str[i]] = 0;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
