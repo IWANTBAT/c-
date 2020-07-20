d8f6e32d260817728efd81dae653fc4676b9d189
IWANTBAT <18729932977@163.com>
2020-07-20 12:59:04 +0800

    算法练习

---
+++ 算法 二维数组中的查找.c
@@ -0,0 +1,20 @@
+﻿
+class Solution {
+public:
+	bool Find(int target, vector<vector<int> > array) {
+		int i = 0;
+		int j = array[0].size() - 1;
+		while (i<array.size() && j >= 0){
+			if (target < array[i][j]){//排除当前列
+				j--;
+			}
+			else if (target > array[i][j]){//排除当前行
+				i++;
+			}
+			else{
+				return true;
+			}
+		}
+		return false;//while条件不成立时说明没找到
+	}
+};
