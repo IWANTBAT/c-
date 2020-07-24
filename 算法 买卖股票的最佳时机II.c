26c613de47ce150d456da253f17eaabae8593bd4
IWANTBAT <18729932977@163.com>
2020-07-24 22:55:02 +0800

    算法训练 贪心算法

---
+++ 算法 买卖股票的最佳时机II.c
@@ -0,0 +1,14 @@
+﻿class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int result = 0;
+		int len = prices.size() - 1;
+		for (int i = 0; i<len; i++){
+			int temp = prices[i + 1] - prices[i];
+			if (temp > 0){
+				result += temp;
+			}
+		}
+		return result;
+	}
+};
