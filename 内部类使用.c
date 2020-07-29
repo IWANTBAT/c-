d1a768a67fe10719cf02b054fb00d6628200c30f
IWANTBAT <18729932977@163.com>
2020-07-29 18:53:53 +0800

    利用内部类的特性进行加法

---
+++ 内部类使用.c
@@ -0,0 +1,23 @@
+﻿class Solution {
+
+public:
+	class Sum{
+	public:
+		Sum(){
+			_sum += _num;
+			_num++;
+		}
+	};
+	int Sum_Solution(int n) {
+		_sum = 0;
+		_num = 1;
+		Sum sumarra[n];
+		return _sum;
+	}
+private:
+	static int _sum;
+	static int _num;
+
+};
+int Solution::_sum = 0;
+int Solution::_num = 1;
