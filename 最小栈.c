595757388441c2d0d644f204df06ab64f7d3a74b
IWANTBAT <18729932977@163.com>
2020-08-06 20:14:13 +0800

    用两个栈实现最小栈

---
+++ 最小栈.c
@@ -0,0 +1,30 @@
+﻿class MinStack {
+public:
+	stack<int> st;
+	stack<int> Minst;
+	/** initialize your data structure here. */
+	MinStack() {
+
+	}
+
+	void push(int x) {
+		if (Minst.empty() || x <= Minst.top())
+			Minst.push(x);
+		st.push(x);
+	}
+
+	void pop() {
+		if (st.top() == Minst.top())
+			Minst.pop();
+		st.pop();
+	}
+
+	int top() {
+		return st.top();
+
+	}
+
+	int getMin() {
+		return Minst.top();
+	}
+};
