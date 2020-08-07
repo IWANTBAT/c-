9b9a8560e166376e7c2f031586b323d3f9bc52ca
IWANTBAT <18729932977@163.com>
2020-08-07 12:17:14 +0800

    使用栈来验证压栈和出栈序列

---
+++ 压栈和出栈序列匹配.c
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+	bool IsPopOrder(vector<int> pushV, vector<int> popV) {
+
+		//用一个栈来模拟出栈的序列，最后判断栈是否为空
+		stack<int> sta;
+		int push = 0;
+		int pop = 0;
+		while (push<pushV.size()){
+			//先入栈
+			sta.push(pushV[push++]);
+
+			//判断是否需要出栈
+			while (!sta.empty() && sta.top() == popV[pop]){
+				sta.pop();
+				pop++;
+			}
+		}
+		return sta.empty();
+	}
+};
