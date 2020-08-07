2eb3366943f548d807f47cad3a096802bb681e5b
IWANTBAT <18729932977@163.com>
2020-08-07 14:42:05 +0800

    用栈实现逆波兰表达式求值

---
+++ 逆波兰表达式.c
@@ -0,0 +1,33 @@
+﻿class Solution {
+public:
+	int evalRPN(vector<string>& tokens) {
+		stack<int> res;
+
+		for (auto & str : tokens){
+			if (str == "+" || str == "-" || str == "*" || str == "/"){
+				int right = res.top();
+				res.pop();
+				int left = res.top();
+				res.pop();
+
+				if (str == "+"){
+					res.push(left + right);
+				}
+				if (str == "-"){
+					res.push(left - right);
+				}
+				if (str == "*"){
+					res.push(left * right);
+				}
+				if (str == "/"){
+					res.push(left / right);
+				}
+			}
+			//不是运算符就是操作数
+			else{
+				res.push(stoi(str));
+			}
+		}
+		return res.top();
+	}
+};
