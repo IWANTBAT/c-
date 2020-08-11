11f3e3bcfdebffd7125d45c6da065017f4a2f6fe
IWANTBAT <18729932977@163.com>
2020-08-11 15:45:07 +0800

    将二叉搜索树修改为双向排序链表
    根据二叉搜索树中序遍历有序的特点

---
+++ 剑指offer  二叉搜索树与双向链表.c
@@ -0,0 +1,35 @@
+﻿/*
+struct TreeNode {
+int val;
+struct TreeNode *left;
+struct TreeNode *right;
+TreeNode(int x) :
+val(x), left(NULL), right(NULL) {
+}
+};*/
+class Solution {
+public:
+	void convers(TreeNode* root, TreeNode** prev){
+		if (root == nullptr)
+			return;
+		convers(root->left, prev);
+		root->left = *prev;
+		if (*prev)
+			(*prev)->right = root;
+		*prev = root;
+		convers(root->right, prev);
+	}
+	TreeNode* Convert(TreeNode* pRootOfTree)
+	{
+		TreeNode* head = pRootOfTree;
+		if (head == nullptr){
+			return head;
+		}
+		TreeNode* prev = nullptr;
+		convers(head, &prev);
+		while (head->left){
+			head = head->left;
+		}
+		return head;
+	}
+};
