9bda469079a5d8c5aa30974345b891876346b4b2
IWANTBAT <18729932977@163.com>
2020-08-11 23:38:10 +0800

    用栈和vector来实现二叉树的前序遍历 栈保存各个根节点 先将当前栈顶节点的值插入vector，再插入左子树的，再右子树的

---
+++ 算法 二叉树的前序遍历（迭代）.c
@@ -0,0 +1,28 @@
+﻿/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<int> preorderTraversal(TreeNode* root) {
+		vector<int> res;
+		stack<TreeNode*> s;
+		s.push(root);
+		while (!s.empty()){
+			TreeNode* temp = s.top();
+			s.pop();
+
+			if (temp == nullptr)
+				continue;
+			res.push_back(temp->val);
+			s.push(temp->right);
+			s.push(temp->left);
+		}
+		return res;
+	}
+};
