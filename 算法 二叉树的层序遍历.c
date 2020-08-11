49c9e226970d5384045932be4310ef9925eb7e4c
IWANTBAT <18729932977@163.com>
2020-08-11 12:32:19 +0800

    用队列实现二叉树的层序遍历，将结果保存在二维vector中

---
+++ 算法 二叉树的层序遍历.c
@@ -0,0 +1,38 @@
+﻿/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		queue<TreeNode*> q;
+		vector<vector<int>> res;
+
+		if (root == nullptr){
+			return res;
+		}
+		q.push(root);
+		while (!q.empty()){
+			int size = q.size();
+			vector<int> v;
+			while (size-- > 0){
+				TreeNode* cur = q.front();
+				q.pop();
+				v.push_back(cur->val);
+				if (cur->left){
+					q.push(cur->left);
+				}
+				if (cur->right){
+					q.push(cur->right);
+				}
+			}
+			res.push_back(v);
+		}
+		return res;
+	}
+};
