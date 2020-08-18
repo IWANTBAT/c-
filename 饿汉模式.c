dfc13a6802dce38ec4a8009b5f24a0366537a598
IWANTBAT <18729932977@163.com>
2020-08-19 01:08:30 +0800

    单例模式---->饿汉模式

---
+++ 饿汉模式.c
@@ -0,0 +1,19 @@
+﻿
+
+class singleton{
+public:
+	static singleton& Get(){
+		return _single;
+	}
+private:
+	singleton()
+	{}
+
+	singleton(const singleton&) = delete;
+
+	singleton& operator=(const singleton&) = delete;
+
+	static singleton _single;
+};
+
+singleton singleton::_single;
