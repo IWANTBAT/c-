7768478076550f40c1d72b5931ecd1a869a7b85e
IWANTBAT <18729932977@163.com>
2020-08-19 20:33:26 +0800

    单例模式----> 懒汉模式

---
+++ 懒汉模式、.c
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<thread>
+#include<mutex>
+using namespace std;
+
+
+class singleton{
+public:
+	static singleton* Get(){
+		//双检查提高效率
+		if (_single == nullptr)
+		{
+			_mux.lock();
+			if (_single == nullptr)
+				_single = new singleton;
+			_mux.unlock();
+		}
+		return _single;
+	}
+private:
+	singleton()
+	{}
+
+	singleton(const singleton&) = delete;
+
+	singleton& operator=(const singleton&) = delete;
+
+	static singleton* _single;
+
+	static mutex _mux;
+};
+
+singleton* singleton::_single = nullptr;
+
+mutex singleton::_mux;
