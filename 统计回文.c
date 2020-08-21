9aae521f1780d49528296451882fb00be27de002
IWANTBAT <18729932977@163.com>
2020-08-22 00:05:23 +0800

    练习

---
+++ 统计回文.c
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool isHW(string str){
+	int len = str.length() - 1;
+	int i = 0;
+	while (i <= len){
+		if (str[i] != str[len])
+			return false;
+		i++;
+		len--;
+	}
+	return true;
+}
+int main()
+{
+	string str1, str2;
+	getline(cin, str1);
+	getline(cin, str2);
+	int count = 0;
+	for (int i = 0; i <= str1.size(); i++){
+		string str = str1;
+		str.insert(i, str2);
+		if (isHW(str))
+			count++;
+	}
+	cout << count << endl;
+	return 0;
+}
