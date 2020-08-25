e14ac862d7cf9f6f60ea7b986effebe7a932a1c8
IWANTBAT <18729932977@163.com>
2020-08-25 21:38:38 +0800

    括号匹配--->栈

---
+++ 合法括号.c
@@ -0,0 +1,26 @@
+﻿class Parenthesis {
+public:
+	bool chkParenthesis(string A, int n) {
+		// write code here
+		stack<char> res;
+		for (auto ch : A)
+		{
+			switch (ch){
+			case'(':
+				res.push(ch);
+				break;
+			case')':
+			{
+					   if (res.empty() || res.top() != '(')
+						   return false;
+					   else
+						   res.pop();
+			}
+				break;
+			default:
+				return false;
+			}
+		}
+		return true;
+	}
+};
