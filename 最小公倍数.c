02f9018ca54e0d7bb6c2ab3848ae69c3648dfa11
IWANTBAT <18729932977@163.com>
2020-08-26 13:03:32 +0800

    最小公倍数 = 乘积 /  最大公约数

---
+++ 最小公倍数.c
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+	int r;
+	while (r = a%b){
+		a = b;
+		b = r;
+	}
+	return b;
+}
+
+int main()
+{
+	int a, b;
+	while (cin >> a >> b){
+		cout << a*b / gcd(a, b) << endl;
+	}
+	return 0;
+}
