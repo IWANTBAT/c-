3efbf7e119dc71192b59e5f89c6036b571259265
IWANTBAT <18729932977@163.com>
2020-09-22 20:27:08 +0800

    奇数在前偶数在后且相对位置不变

---
+++ 计数位于偶数前且相对位置不变.c
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	void reOrderArray(vector<int> &array) {
+		int k = 0;
+		for (int i = 0; i<array.size(); i++){
+			if (array[i] & 1){
+				int temp = array[i];
+				int j = i;
+				while (j>k){
+					array[j] = array[j - 1];
+					j--;
+				}
+				array[k++] = temp;
+			}
+		}
+	}
+};
